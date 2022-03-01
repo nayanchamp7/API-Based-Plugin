301247d3e0073c9a2916dd005d2b315d88376385
Nazrul Islam <nazrul.webappick@gmail.com>
2022-03-01 17:10:33 +0600

    added shorcode

--- includes/ABP.php
+++ includes/ABP.php
@@ -17,6 +17,7 @@ class ABP {
      */
     public function __construct() {
         add_action( 'init', array( $this, 'init' ) );
+        add_shortcode( 'am_users', array( $this, 'show_users_front' ) );
     }
 
     /**
@@ -26,5 +27,16 @@ class ABP {
         $admin = new Admin();
         $assets = new Assets();
 
+    }
+
+    /**
+     * Shortcode to show users in front
+     */
+    public function show_users_front() {
+        ob_start();
+
+        include  ABP_FILE_DIR . '/templates/admin/abp-lists.php';
+
+        return ob_get_clean();
     }
 }
